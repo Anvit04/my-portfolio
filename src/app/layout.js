92470e30e3f7da7c89f4412a396cc4b076c46ad5
anvit <anvit.urja360@gmail.com>
2025-06-20 14:32:43 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,3 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata = {
-//   title: "Anvit",
-//   description: "The Ultimate Web Designer",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <link rel="preconnect" href="https://fonts.googleapis.com" />
-//         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-//           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-//           <body >
-//             {children}
-//           </body>
-//         </html>
-//         );
-// }
-
-
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 
@@ -46,12 +14,12 @@ const geistMono = Geist_Mono({
 const roboto = Roboto({
   subsets: ["latin"],
   variable: "--font-roboto",
-  weight: ["400", "700"], // you can include other weights as needed
+  weight: ["400", "700"],
 });
 
 export const metadata = {
-  title: "Anvit",
-  description: "The Ultimate Web Designer",
+  title: "Anvit - The Ultimate Web Designer",
+  description: "Crafting exceptional digital experiences with 3+ years of expertise in modern web technologies. From responsive designs to full-stack applications, I bring ideas to life with pixel-perfect precision.",
 };
 
 export default function RootLayout({ children }) {
@@ -63,4 +31,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
