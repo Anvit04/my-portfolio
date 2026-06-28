ca093f654201551069187296f503c1deaa0a0e73
anvit <anvit.urja360@gmail.com>
2026-06-28 12:52:21 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,6 +42,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable}`}>
+      <meta name="google-site-verification" content="Fb-QgDlxz9RxnsVZ5kFtEp6HaxuGqtGEb6VijaLOua4" />
       <body>
         {children}
 
